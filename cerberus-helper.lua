b528aa70e12fa5b66653be0ce3d3c0715864f772
Andergr0ynd <107173987+Andergr0ynd@users.noreply.github.com>
2024-09-22 14:26:18 +0500

    Update cerberus-helper.lua

--- cerberus-helper.lua
+++ cerberus-helper.lua
@@ -1,6 +1,6 @@
 script_name("cerberus-helper")
 script_url("https://github.com/Andergr0ynd/luaupdate/")
-script_version("22.09.2024.13.55")
+script_version("22.09.2024.14.29")
 
 local enable_autoupdate = true -- false to disable auto-update + disable sending initial telemetry (server, moonloader version, script version, samp nickname, virtual volume serial number)
 local autoupdate_loaded = false
